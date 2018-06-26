b0051a88fadb3bc664d14a2c0930bc4dbc2e178a
gabantesp <40573258+gabantesp@users.noreply.github.com>
2018-06-26 11:02:19 -0400

    Update __init__.py

--- gym_PhaseOne/envs/__init__.py
+++ gym_PhaseOne/envs/__init__.py
@@ -1,8 +1,3 @@
 from gym_PhaseOne.envs.PhaseOne_env import PhaseOneEnv
 from gym_PhaseOne.envs.PhaseOne_extrahard_env import PhaseOneExtraHardEnv
-from gym.envs.registration import register 
 
-register(
-  id = 'PhaseOne-v0',
-  entry_point = 'PhaseOne.PhaseOne:PhaseOne',
-)
